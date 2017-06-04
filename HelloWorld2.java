5fe8a6da18280eb8e76f9bd1d917cc6ba1c95d44
WilwilBRW <walidbrw40345675@gmail.com>
2017-06-04 09:31:52 +0100

    Create HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,20 @@
+public class HelloWorld2{
+
+
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  
+}
